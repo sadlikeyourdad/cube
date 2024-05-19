ad35611fa77c35c7360abc435418ef0d3e39bd97
sadlikeyourdad <57995714+sadlikeyourdad@users.noreply.github.com>
2024-05-19 19:28:50 +0200

    Update script.js

--- script.js
+++ script.js
@@ -38,3 +38,15 @@ document.addEventListener('touchmove', (e) => {
 document.addEventListener('touchend', () => {
   isDragging = false;
 });
+// script.js
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(registration => {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, err => {
+        console.log('ServiceWorker registration failed: ', err);
+      });
+  });
+}
+
