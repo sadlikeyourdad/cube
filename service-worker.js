0eba314ab3f52ef18a99f794b4c1a8c9b3590960
sadlikeyourdad <57995714+sadlikeyourdad@users.noreply.github.com>
2024-05-19 19:35:20 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,40 +1,21 @@
-// service-worker.js
-const cacheName = '3d-cube-cache-v1';
-const assetsToCache = [
-    '/',
-    '/index.html',
-    '/styles.css',
-    '/script.js',
-    '/manifest.json',
-    '/icon-192.png',
-    '/icon-512.png'
-];
-
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open(cacheName)
-            .then(cache => cache.addAll(assetsToCache))
-            .then(self.skipWaiting())
+        caches.open('v1').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/styles.css',
+                '/app.js',
+                '/manifest.json'
+            ]);
+        })
     );
 });
 
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cache => {
-                    if (cache !== cacheName) {
-                        return caches.delete(cache);
-                    }
-                })
-            );
-        }).then(self.clients.claim())
-    );
-});
-
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetch(event.request))
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
     );
 });
