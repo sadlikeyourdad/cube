965b96abff1da1d706a81e7a57dc23c1969283c5
sadlikeyourdad <57995714+sadlikeyourdad@users.noreply.github.com>
2024-05-19 19:14:03 +0200

    Update service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+// service-worker.js
+const cacheName = '3d-cube-cache-v1';
+const assetsToCache = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/script.js',
+    '/manifest.json',
+    '/icon-192.png',
+    '/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(cacheName)
+            .then(cache => cache.addAll(assetsToCache))
+            .then(self.skipWaiting())
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== cacheName) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        }).then(self.clients.claim())
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => response || fetch(event.request))
+    );
+});
